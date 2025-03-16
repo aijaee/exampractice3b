d3ae800ce403a3bfb80ea8b4f1b50f8503ae01ca
Irad Joseph Arante <63544798+aijaee@users.noreply.github.com>
2025-03-16 20:53:37 +0800

    Integrated Bootstrap

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/tests.css') }}">
 </head>
 <body>
-    <div class="table_component" role="region" tabindex="0">
-        <table>
-            <caption>Table 1</caption>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Created at</th>
-                    <th>Updated at</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($tests as $test)
+    <div class="container mt-4">
+        <div class="table-responsive" role="region" tabindex="0">
+            <table class="table table-striped table-bordered">
+                <caption>Table 1</caption>
+                <thead class="table-dark">
                     <tr>
-                        <td>{{ $test->id }}</td>
-                        <td>{{ $test->name }}</td>
-                        <td>{{ $test->created_at }}</td>
-                        <td>{{ $test->updated_at }}</td>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <div>
+                </thead>
+                <tbody>
+                    @foreach($tests as $test)
+                        <tr>
+                            <td>{{ $test->id }}</td>
+                            <td>{{ $test->name }}</td>
+                            <td>{{ $test->created_at }}</td>
+                            <td>{{ $test->updated_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
